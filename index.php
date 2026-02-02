3149486de7e9251e17687903306bf3fa9d273224
ivantucakov <itucakov@gmail.com>
2026-02-01 23:54:06 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><style type="text/css">
+.MyText {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+}
+</style>
+<head>
+
+
+<title>Ivan Tucakov and Cultura Rasa</title>
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<style type="text/css">
+.MyTitle {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+}
+.MyTitle {
+	font-size: 16px;
+}
+.MyTitle {
+	font-size: 18px;
+	font-weight: bold;
+}
+.MySubTitle {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 16px;
+	font-weight: bold;
+	text-align: center;
+}
+
+.MyImage {
+  width: 600px;        /* fixed target size */
+  max-width: 100%;     /* makes it responsive */
+  height: auto;        /* keeps proportions */
+  display: block;      
+  margin: 0 auto;      /* centers it */
+}
+
+img {
+  max-width: 100%;
+  width: 100%;
+  height: auto;
+  display: block;
+  margin: 0 auto;
+}
+
+iframe {
+  width: 100%;
+  max-width: 100%;
+  min-height: 400px;
+}
+
+
+</style>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+</head>
+
+<body>
+
+
+
+</body>
+</html>
